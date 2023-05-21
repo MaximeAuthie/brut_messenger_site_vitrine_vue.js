272513d56a10e559032dbc1879f3c4b04240b2ca
Maxime Authié <authie.maxime@orange.fr>
2023-05-21 19:09:27 +0200

    Ajout service d'inscription

---
+++ src/services/UserDataService.js
@@ -0,0 +1,31 @@
+class UserDataService {
+    
+    async addUser(userData) {
+        let message = {};
+        const bodyJson = JSON.stringify(userData);
+        const response = await fetch('https://127.0.0.1:8000/api/user/add', {
+            method:'POST',
+            header: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: bodyJson,
+        })
+
+        switch (response.status) {
+            case 200:
+                message = {code: response.status, text: 'Félicitations ' + userData.firstName + '. Votre compte a bien été créé!'};
+                break;
+            case 206:
+                message = {code: response.status, text: 'L\'adresse mail ' + userData.email + ' est déjà associée à un autre compte.'};
+                break;
+            default:
+                message = {code: response.status, text: 'Erreur de connexion au serveur. Veuillez réessayer plus tard.'};
+        }
+        
+        return message;
+
+    }
+}
+
+export default new UserDataService();
