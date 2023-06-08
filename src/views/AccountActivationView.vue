213c77367b743e8d88d710d88010710308138d50
Maxime Authié <authie.maxime@orange.fr>
2023-06-08 14:08:29 +0200

    Gestion du param "code erreur" renvoyé par l'api

--- src/views/AccountActivationView.vue
+++ src/views/AccountActivationView.vue
@@ -3,9 +3,9 @@
     <router-link to="/home"><img src="../../public/medias/back.png" alt="Retour à l'accueil du site" id="return-icon" title="Retour à l'accueil du site"/></router-link>
   
     <div id="content-body">
-      <div id="not-found">
-        <h1 class="titre-page">J'aime la galette.</h1>
-        <h2><router-link to="/home">Retour à l'acceuil du site</router-link></h2>
+      <div id="not-found" :style="{backgroundColor : success ? '#23A093' : '#F55A4F'}">
+        <h1 class="titre-page" > {{ defineMessage() }} </h1>
+        <h2 ><router-link to="/home" class="home-return">Retour à l'acceuil du site</router-link></h2>
       </div>
     </div>
   
@@ -16,12 +16,33 @@
     data() {
         return {
             url: new URLSearchParams(document.location.href),
-            code : 0
+            code : 0,
+            success: false,
+            message: ''
+        }
+    },
+    methods: {
+        defineMessage() {
+            switch (this.code) {
+                case '200':
+                    this.message = "Félicitations! Votre compte a été activé avec succès! Vous pouvez dès maintenant vous connecter sur votre mobile!";
+                    this.success = true;
+                    break;
+                case '206':
+                    this.message = "Le compte a déjà été activé.";
+                    break;
+                case '498':
+                    this.message = "Le délai de validité du lien est dépassé. Un nouveau lien de confirmation vient de vous être envoyé sur l'adresse mail saisie lors de votre inscription";
+                    break;
+                default:
+                    this.message = "Une erreur est survenue lors de l'activation de votre compte. Veuillez réessayer plus tard.";
+                    break;
+            }
+            return this.message;
         }
     },
     mounted(){
         this.code = this.$route.params.status;
-
     }
   }
   </script>
