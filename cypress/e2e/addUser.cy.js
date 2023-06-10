923e81bef21d3ba1448d181922bb1920db261d51
Maxime Authié <authie.maxime@orange.fr>
2023-06-10 17:57:08 +0200

    Ajout tests password

--- cypress/e2e/addUser.cy.js
+++ cypress/e2e/addUser.cy.js
@@ -1,13 +1,13 @@
 Cypress.config('defaultCommandTimeout', 5000);
-describe('template spec', () => {
+describe('addUserForm', () => {
   it('success', () => {
     cy.visit('http://localhost:8080/register');
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#birth').type('1989-06-21');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#success').should('contain', 'Votre compte a bien été créé!');
   }),
@@ -17,8 +17,8 @@ describe('template spec', () => {
     cy.get('#last-name').type('Authié');
     cy.get('#birth').type('1989-06-21');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#serverError').should('contain', 'est déjà associée à un autre compte.');
   }),
@@ -37,8 +37,8 @@ describe('template spec', () => {
     cy.get('#last-name').type('Authié');
     cy.get('#birth').type('1989-06-21');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#firstNameEmpty').should('contain', 'Veuillez saisir votre prénom');
   }),
@@ -47,8 +47,8 @@ describe('template spec', () => {
     cy.get('#first-name').type('Maxime');
     cy.get('#birth').type('1989-06-21');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#lastNameEmpty').should('contain', 'Veuillez saisir votre nom');
   }),
@@ -57,8 +57,8 @@ describe('template spec', () => {
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#birthdayEmpty').should('contain', 'Veuillez saisir votre date de naissance');
   }),
@@ -66,8 +66,8 @@ describe('template spec', () => {
     cy.visit('http://localhost:8080/register');
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('1234');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#emailEmpty').should('contain', 'Veuillez saisir votre adresse mail');
   }),
@@ -76,38 +76,38 @@ describe('template spec', () => {
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#re-password').type('1234');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#passwordEmpty').should('contain', 'Veuillez saisir un mot de passe');
-    cy.get('#passwordsError').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
+    cy.get('#passwordsMatch').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
   }),
   it('error-empty-re-password', () => {
     cy.visit('http://localhost:8080/register');
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
+    cy.get('#password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#rePasswordEmpty').should('contain', 'Veuillez ressaisir votre mot de passe');
-    cy.get('#passwordsError').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
+    cy.get('#passwordsMatch').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
   }),
   it('error-different-passwords', () => {
     cy.visit('http://localhost:8080/register');
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maxime@orange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('12345');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty999');
     cy.get('.principal').click();
-    cy.get('#passwordsError').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
+    cy.get('#passwordsMatch').should('contain', 'Les deux mots de passe saisis ne sont pas identiques!');
   }),
   it('error-email-format-.fr', () => {
     cy.visit('http://localhost:8080/register');
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maxime@orange');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('12345');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#emailError').should('contain', 'Adresse mail incorrecte');
   }),
@@ -116,9 +116,53 @@ describe('template spec', () => {
     cy.get('#first-name').type('Maxime');
     cy.get('#last-name').type('Authié');
     cy.get('#email').type('authie.maximeorange.fr');
-    cy.get('#password').type('1234');
-    cy.get('#re-password').type('12345');
+    cy.get('#password').type('Azerty99');
+    cy.get('#re-password').type('Azerty99');
     cy.get('.principal').click();
     cy.get('#emailError').should('contain', 'Adresse mail incorrecte');
+  }),
+  it('error-password-uppercase', () => {
+    cy.visit('http://localhost:8080/register');
+    cy.get('#first-name').type('Maxime');
+    cy.get('#last-name').type('Authié');
+    cy.get('#email').type('authie.maxime@orange.fr');
+    cy.get('#password').type('azerty99');
+    cy.get('#re-password').type('azerty99');
+    cy.get('.principal').click();
+    cy.get('#passwordError').should('contain', 'Le mot de passe doit contenir : ');
+    cy.get('#containUppercase').should('contain', 'Une lettre majuscule');
+  }),
+  it('error-password-lowercase', () => {
+    cy.visit('http://localhost:8080/register');
+    cy.get('#first-name').type('Maxime');
+    cy.get('#last-name').type('Authié');
+    cy.get('#email').type('authie.maxime@orange.fr');
+    cy.get('#password').type('AZERTY99');
+    cy.get('#re-password').type('AZERTY99');
+    cy.get('.principal').click();
+    cy.get('#passwordError').should('contain', 'Le mot de passe doit contenir : ');
+    cy.get('#containLowercase').should('contain', 'Une lettre minuscule');
+  }),
+  it('error-password-number', () => {
+    cy.visit('http://localhost:8080/register');
+    cy.get('#first-name').type('Maxime');
+    cy.get('#last-name').type('Authié');
+    cy.get('#email').type('authie.maxime@orange.fr');
+    cy.get('#password').type('AzertyUU');
+    cy.get('#re-password').type('AzertyUU');
+    cy.get('.principal').click();
+    cy.get('#passwordError').should('contain', 'Le mot de passe doit contenir : ');
+    cy.get('#containNumber').should('contain', 'Un chiffre');
+  }),
+  it('error-password-characters', () => {
+    cy.visit('http://localhost:8080/register');
+    cy.get('#first-name').type('Maxime');
+    cy.get('#last-name').type('Authié');
+    cy.get('#email').type('authie.maxime@orange.fr');
+    cy.get('#password').type('Azerty9');
+    cy.get('#re-password').type('Azerty9');
+    cy.get('.principal').click();
+    cy.get('#passwordError').should('contain', 'Le mot de passe doit contenir : ');
+    cy.get('#isLongEnough').should('contain', '8 caractères minimum');
   })
 })
